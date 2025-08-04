8631dc545a38fec96196bdd0637363287c6809f7
MelodicCougar7 <melodiccougar7@gmail.com>
2025-08-04 18:17:27 -0400

    Polishing, yet no success

--- src/main/java/org/github/melodiccougar7/ecologic_fixes/mixin/CoconutSliceNerf.java
+++ src/main/java/org/github/melodiccougar7/ecologic_fixes/mixin/CoconutSliceNerf.java
@@ -5,15 +5,19 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import samebutdifferent.ecologics.item.CoconutSliceItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import samebutdifferent.ecologics.item.CoconutSliceItem;
 import samebutdifferent.ecologics.registry.ModItems;
 
 @MethodsReturnNonnullByDefault
 @Mixin(CoconutSliceItem.class)
-public abstract class CoconutSliceNerf {
+public class CoconutSliceNerf extends Item {
+
+    public CoconutSliceNerf(Properties settings) {
+        super(settings);
+    }
 
     /**
      * @author MC7
