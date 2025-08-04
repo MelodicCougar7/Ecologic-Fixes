512c55a954f8966fc417b7172d431a2e90e89d99
MelodicCougar7 <melodiccougar7@gmail.com>
2025-08-04 17:22:33 -0400

    Initial Commit (broken state)
    Initial Commit (broken state).

--- src/main/java/org/github/melodiccougar7/ecologic_fixes/mixin/CoconutSliceNerf.java
+++ src/main/java/org/github/melodiccougar7/ecologic_fixes/mixin/CoconutSliceNerf.java
@@ -1,4 +1,42 @@
 package org.github.melodiccougar7.ecologic_fixes.mixin;
 
-public class CoconutSliceNerf {
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import samebutdifferent.ecologics.item.CoconutSliceItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import samebutdifferent.ecologics.registry.ModItems;
+
+@MethodsReturnNonnullByDefault
+@Mixin(CoconutSliceItem.class)
+public abstract class CoconutSliceNerf {
+
+    /**
+     * @author MC7
+     * @reason Terribly sorry, but some things just need to work before they are refactored later
+     */
+
+    @Overwrite
+
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        if (!pLevel.isClientSide) {
+            if (pLivingEntity instanceof Player player) {
+                // player.removeAllEffects(); // behavior is the same, this is removed
+                ItemStack mainHandStack = player.getMainHandItem();
+                ItemStack coconutHuskStack = new ItemStack(ModItems.COCONUT_HUSK.get());
+                if (!player.getAbilities().instabuild) {
+                    if (!mainHandStack.isEmpty()) {
+                        if (!player.getInventory().add(coconutHuskStack.copy())) {
+                            player.drop(coconutHuskStack, false);
+                        }
+                    }
+                }
+            }
+        }
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
+    }
 }
